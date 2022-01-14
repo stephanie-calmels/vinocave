16499aa9dace434ec57b0eb0ebb15d31e3848bac
stephanie-calmels <stephcalmels82@gmail.com>
2022-01-14 13:46:43 +0100

    mainController

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 
+const router = require('./app/router');
+
 const app = express();
+app.use(router);
 
 const PORT = process.env.PORT || 3000;
 
