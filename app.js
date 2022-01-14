6ec97f6823bf6de8009538347a3e048e48291d6b
stephanie-calmels <stephcalmels82@gmail.com>
2022-01-14 15:50:57 +0100

    bottleController

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const router = require('./app/router');
 
 const app = express();
+
+app.use(express.json());
 app.use(router);
 
 const PORT = process.env.PORT || 3000;
--- app/router.js
+++ app/router.js
@@ -2,7 +2,24 @@ const express = require('express');
 const router = express.Router();
 
 const mainController = require('./controllers/mainController');
+const bottleController = require('./controllers/bottleController');
 
+// page d'accueil
 router.get('/', mainController.home);
+
+// détail et suppression d'une bouteille
+router.get('/bouteille/:id', bottleController.getOneBottle);
+router.delete('/bouteille/:id', bottleController.deleteBottle);
+
+// affichage du formulaire de modification d'une bouteille
+router.get('/maj-bouteille/:id', bottleController.modifyBottle);
+// traitement du formulaire
+router.post('/maj-bouteille/:id', bottleController.updateBottle);
+
+
+// affichage du formulaire d'ajout de bouteille
+router.get('/nouvelle_bouteille', bottleController.addBottle);
+// traitement du formulaire
+router.post('/nouvelle_bouteille', bottleController.newBottle);
 
 module.exports = router;
