a152bb38fd4b37a19e2e5865aa4a32da05d42e5b
stephanie-calmels <stephcalmels82@gmail.com>
2022-01-17 11:07:57 +0100

    install ejs

--- app.js
+++ app.js
@@ -5,7 +5,17 @@ const router = require('./app/router');
 
 const app = express();
 
+// ressources statiques
+app.use(express.static('./public'));
+
+// gestion des templates
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+
+// middlewares
 app.use(express.json());
+
+// router
 app.use(router);
 
 const PORT = process.env.PORT || 3000;
