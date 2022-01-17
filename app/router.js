c5ce2d6a67b31b17f453200b875c7d1bbb95548f
stephanie-calmels <stephcalmels82@gmail.com>
2022-01-17 10:55:14 +0100

    appellationController

--- app/router.js
+++ app/router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const mainController = require('./controllers/mainController');
 const bottleController = require('./controllers/bottleController');
+const appellationController = require('./controllers/appellationController');
 
 // page d'accueil
 router.get('/', mainController.home);
@@ -16,10 +17,15 @@ router.get('/maj-bouteille/:id', bottleController.modifyBottle);
 // traitement du formulaire
 router.post('/maj-bouteille/:id', bottleController.updateBottle);
 
-
 // affichage du formulaire d'ajout de bouteille
 router.get('/nouvelle_bouteille', bottleController.addBottle);
 // traitement du formulaire
 router.post('/nouvelle_bouteille', bottleController.newBottle);
+
+// liste des appellations
+router.get('/appellation', appellationController.getAppellations);
+
+// détail d'une appellation 
+router.get('/appellation/:id', appellationController.getOneAppellation);
 
 module.exports = router;
