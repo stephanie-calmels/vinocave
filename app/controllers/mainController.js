fd0f24758371141cb4e3d0bab7352f14dd09cabb
stephanie-calmels <stephcalmels82@gmail.com>
2022-01-21 13:29:29 +0100

    affichage des bouteilles dont quantité > 0 par défaut sur page accueil

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,8 +1,14 @@
 const { Bottle } = require('../models');
+const { Op } = require('sequelize');
 
 module.exports = {
   home: (request, response) => {
     Bottle.findAll({
+      where: {
+        quantity: {
+          [Op.gt]: 0,
+        }
+      },
       include: 'appellation'
     }).then(bottles => {
       response.render('home', {bottles});
