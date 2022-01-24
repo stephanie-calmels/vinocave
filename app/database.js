b6798c537250d857908cea59a8521b699ec04b97
stephanie-calmels <stephcalmels82@gmail.com>
2022-01-24 11:44:46 +0100

    changement config sequelize ssl

--- app/database.js
+++ app/database.js
@@ -8,8 +8,10 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
   dialect: 'postgres',
   dialectOptions: {
-    ssl: true
-  }
-});
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },});
 
 module.exports = sequelize;
