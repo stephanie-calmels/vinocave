52863e883ff748c0505549308c1646e540a83f95
stephanie-calmels <stephcalmels82@gmail.com>
2022-11-26 16:09:19 +0100

    modification affichage

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -27,39 +27,58 @@ module.exports = {
     });
   },
 
-  colorFilter: (request, response) => {
-    const color = request.params.color;
-    let type = '';
-    if (color === 'blanc') {
-      type = 'blancs'
-    }
-    if (color === 'rose') {
-      type = 'rosés'
-    }
-    if (color === 'rouge') {
-      type = 'rouges'
-    }
+  colorFilter: async (request, response, next) => {
+    try {
+      const color = request.params.color;
+      let type = '';
+      if (color === 'blanc') {
+        type = 'blancs'
+      }
+      if (color === 'rose') {
+        type = 'rosés'
+      }
+      if (color === 'rouge') {
+        type = 'rouges'
+      }
 
-    Bottle.findAll({
-      where: {
-        color
-      },
-      include: 'appellation',
-      order: [
-        ['guard', 'ASC'],
-        ['appellation_id', 'ASC'],
-        ['quantity', 'DESC'],
-      ]
-    }).then(bottles => {
+      const stockedBottles = await Bottle.findAll({
+        where: {
+          color,
+          quantity: {
+            [Op.gt]: 0,
+          }
+        },
+        include: 'appellation',
+        order: [
+          // ['guard', 'ASC'],
+          ['appellation', 'label', 'ASC'],
+          ['millesime', 'ASC'],
+          ['quantity', 'DESC'],
+        ]
+      });
+  
+      const unstockedBottles = await Bottle.findAll({
+        where: {
+          color,
+          quantity: 0
+        },
+        include: 'appellation',
+        order: [
+          ['appellation', 'label', 'ASC'],
+          ['millesime', 'ASC'],
+        ]
+      });
       response.render('home', {
-        bottles,
+        bottles: [...stockedBottles, ...unstockedBottles],
         title: `Tous les vins ${type}`
       });
-    }).catch(error => {
+
+    } catch (error) {
       logger.error(new Error(error))
       response.render('error', {message: 'Une erreur est survenue.'})
-    });
+    }
   },
+  
 
   notFound: (request, response) => {
     response.status(404).render('404', {message: 'Cette page n\'existe pas.'});
